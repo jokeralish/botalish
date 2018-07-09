2c844cedaf3625f6f506c3f762cd6015d22d2e2d
heefpuy <39878411+heefpuy@users.noreply.github.com>
2018-07-09 16:07:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -548,16 +548,16 @@ if ($type == 'join') {
         'actions' =>
         array (
           0 =>
-          array {
+          array (
             'type' => 'uri',
             'label' => 'Creator',
             'uri' => 'https://line.me/ti/p/~heefpuy',
-          },
-          {
+          ),
+          (
             'type' => 'text',
             'label' => 'Perintah',
             'text' => '#menu',
-          },
+          ),
         ),
       ),
     ),
